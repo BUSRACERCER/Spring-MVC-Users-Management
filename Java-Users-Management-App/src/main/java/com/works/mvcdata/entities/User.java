4f737a8448a89b1ef8a92a6bea8f058014aced08
BÜŞRA ÇERÇER <busra_cercer@outlook.com>
2023-06-21 20:25:08 +0300

    SPRING-MVC-USERS-MANEGEMENT

---
+++ Java-Users-Management-App/src/main/java/com/works/mvcdata/entities/User.java
@@ -0,0 +1,17 @@
+package com.works.mvcdata.entities;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private int uid;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private int age;
+    private String date;
+    private String remember;
+
+
+}
